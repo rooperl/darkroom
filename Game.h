eca3e40f672ebd5bc9e73eca375856e8bcd46fb6
rooperl <lindstrom.roope@gmail.com>
2016-09-29 14:25:26 +0300

    renamed

---
+++ Game.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <iostream>
+#include <ctime>
+#include <windows.h>
+#include <chrono>
+#include <thread>
+
+#include "Player.h"
+#include "Map.h"
+
+class Game {
+public:
+	bool quit;
+} game;
+
+void wait(int milliseconds) {
+	std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+}
+
+void fillTextBuffer(int spaces) {
+	for (int space = 0; space <= spaces; space++)
+		std::cout << " ";
+}
+
+void init() {
+	srand((unsigned)time(NULL));
+	CONSOLE_CURSOR_INFO cursorInfo;
+	cursorInfo.dwSize = 1;
+	cursorInfo.bVisible = false;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+	resetMap();
+	map.light = false;
+}
+
+bool windowFocused() {
+	return GetConsoleWindow() == GetForegroundWindow();
+}
+
+bool toggleKey(short VK) {
+	return GetAsyncKeyState(VK) & 0x8000 != 0;
+}
+
+void checkKeyPresses() {
+	if (GetAsyncKeyState(VK_UP)) move(UP);
+	if (GetAsyncKeyState(VK_DOWN)) move(DOWN);
+	if (GetAsyncKeyState(VK_LEFT)) move(LEFT);
+	if (GetAsyncKeyState(VK_RIGHT)) move(RIGHT);
+	if (toggleKey(VK_ESCAPE)) game.quit = true;
+	if (toggleKey('L')) map.light = !map.light;
+	if (toggleKey('R')) resetMap(map.light);
+}
---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include "Game.h"
+
+int main() {
+	init();
+	while (true) {
+		clearScreen();
+		drawMap();
+		if (map.light)
+			std::cout << "You have turned the lights on.";
+		else
+			std::cout << "You're in a dark room.";
+		fillTextBuffer(50);
+		std::cout << "\n\nCoins: " << player.coins << "\n\n";
+		wait(50);
+		if (windowFocused()) checkKeyPresses();
+		if (game.quit) return 0;
+	}
+}
