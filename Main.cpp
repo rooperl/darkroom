59eef795eb7271096b94222fbff481e1a5e3237b
rooperl <lindstrom.roope@gmail.com>
2016-10-02 15:11:42 +0300

    fix

--- Main.cpp
+++ Main.cpp
@@ -13,10 +13,8 @@ int main() {
 	while (true) {
 		setCursor();
 		drawMap(player, map);
-		if (map.light)
-			std::cout << "You have turned the lights on." << fillTextBuffer();
-		else
-			std::cout <<"You're in a dark room." << fillTextBuffer();
+		if (map.light) std::cout << "You have turned the lights on." << fillTextBuffer();
+		else std::cout <<"You're in a dark room." << fillTextBuffer();
 		std::cout << "\n\nCoins: " << player.coins << fillTextBuffer() << "\n\n";
 		wait(50);
 		if (windowFocused()) checkKeyPresses(game, map, player);
