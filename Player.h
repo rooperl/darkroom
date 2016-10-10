19e856128420efc920dc90110dce82e99cde20e1
rooperl <lindstrom.roope@gmail.com>
2016-10-10 21:38:18 +0300

    Update Player.h

--- Player.h
+++ Player.h
@@ -15,21 +15,3 @@ public:
 	short coins;
 	Direction direction;
 };
-=======
-#pragma once
-
-const char PLAYER_UP = '^';
-const char PLAYER_DOWN = 'v';
-const char PLAYER_LEFT = '<';
-const char PLAYER_RIGHT = '>';
-
-enum Direction { UP, DOWN, LEFT, RIGHT };
-
-class Player {
-public:
-	int x;
-	int y;
-	int coins;
-	Direction direction;
-};
->>>>>>> 670d61cc465dd8818df27b98dc19b6bdb4ec15cf
