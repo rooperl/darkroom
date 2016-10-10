a92dde94c17e5878822d0b9699e9272835cadc1e
rooperl <lindstrom.roope@gmail.com>
2016-10-10 21:34:03 +0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -15,21 +15,3 @@ bool toggleKey(short VK);
 void readFile(Map& map, Player& player);
 void writeFile(Map map);
 void checkKeyPresses(Map& map, Player& player);
-
-#pragma once
-#include "Player.h"
-#include "Map.h"
-
-const short GAME_SPEED = 50;
-const std::string FILENAME = "darkroom.drmp";
-const std::string MANUAL_EDIT_TEXT = "MANUAL_EDIT";
-
-void wait(short milliseconds);
-void init(Player& player, Map& map);
-void setCursor(short y = 0, short x = 0);
-std::string fillTextBuffer();
-bool windowFocused();
-bool toggleKey(short VK);
-void readFile(Map& map, Player& player);
-void writeFile(Map map);
-void checkKeyPresses(Map& map, Player& player);
